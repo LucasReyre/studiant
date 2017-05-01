13aa266ee73d5b23c2f8b7db2707abcaaf4c6feb
Lucas Reyre <lucas.reyre@gmail.com>
2017-05-01 22:13:57 +0200

    dismiss form

--- app/src/main/java/com/studiant/com/presentation/ui/activities/AddJobActivity.java
+++ app/src/main/java/com/studiant/com/presentation/ui/activities/AddJobActivity.java
@@ -1,114 +1,15 @@
 package com.studiant.com.presentation.ui.activities;
 
-import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.widget.EditText;
-import android.widget.LinearLayout;
 
 import com.studiant.com.R;
 
-import ernestoyaquello.com.verticalstepperform.*;
-import ernestoyaquello.com.verticalstepperform.interfaces.VerticalStepperForm;
-
-public class AddJobActivity extends AppCompatActivity implements VerticalStepperForm {
-
-    private VerticalStepperFormLayout verticalStepperForm;
-    private EditText name;
-    private EditText email;
+public class AddJobActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_job);
-
-        String[] mySteps = {"Name", "Email", "Phone Number"};
-        int colorPrimary = ContextCompat.getColor(getApplicationContext(), R.color.colorPrimary);
-        int colorPrimaryDark = ContextCompat.getColor(getApplicationContext(), R.color.colorPrimaryDark);
-
-        // Finding the view
-        verticalStepperForm = (VerticalStepperFormLayout) findViewById(R.id.vertical_stepper_form);
-
-        // Setting up and initializing the form
-        VerticalStepperFormLayout.Builder.newInstance(verticalStepperForm, mySteps, this, this)
-                .primaryColor(colorPrimary)
-                .primaryDarkColor(colorPrimaryDark)
-                .displayBottomNavigation(true) // It is true by default, so in this case this line is not necessary
-                .init();
-    }
-
-    @Override
-    public View createStepContentView(int stepNumber) {
-        View view = null;
-        switch (stepNumber) {
-            case 0:
-                view = createNameStep();
-                break;
-            case 1:
-                view = createEmailStep();
-                break;
-            case 2:
-                view = createPhoneNumberStep();
-                break;
-        }
-        return view;
-    }
-
-    private View createNameStep() {
-        // Here we generate programmatically the view that will be added by the system to the step content layout
-        name = new EditText(this);
-        name.setSingleLine(true);
-        name.setHint("Your name");
-        return name;
-    }
-
-    private View createEmailStep() {
-        // In this case we generate the view by inflating a XML file
-        name = new EditText(this);
-        name.setSingleLine(true);
-        name.setHint("Your name");
-        return name;
-    }
-
-    private View createPhoneNumberStep() {
-        name = new EditText(this);
-        name.setSingleLine(true);
-        name.setHint("Your name");
-        return name;
-    }
-
-    @Override
-    public void onStepOpening(int stepNumber) {
-        switch (stepNumber) {
-            case 0:
-                checkName();
-                break;
-            case 1:
-                break;
-            case 2:
-                // As soon as the phone number step is open, we mark it as completed in order to show the "Continue"
-                // button (We do it because this field is optional, so the user can skip it without giving any info)
-                verticalStepperForm.setStepAsCompleted(2);
-                // In this case, the instruction above is equivalent to:
-                // verticalStepperForm.setActiveStepAsCompleted();
-                break;
-        }
-    }
-
-    private void checkName() {
-        if(name.length() >= 3 && name.length() <= 40) {
-            verticalStepperForm.setActiveStepAsCompleted();
-        } else {
-            // This error message is optional (use null if you don't want to display an error message)
-            String errorMessage = "The name must have between 3 and 40 characters";
-            verticalStepperForm.setActiveStepAsUncompleted(errorMessage);
-        }
-    }
-
-    @Override
-    public void sendData() {
-
     }
 }
